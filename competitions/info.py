8e26e226457188495222c2176996830e78fae0a0
Abhishek Thakur <abhishek4@gmail.com>
2024-03-12 19:13:19 +0100

    remove try-except

--- competitions/info.py
+++ competitions/info.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass
 from datetime import datetime
 
 from huggingface_hub import hf_hub_download
-from huggingface_hub.utils._errors import EntryNotFoundError
-from loguru import logger
 
 
 @dataclass
@@ -13,31 +11,24 @@ class CompetitionInfo:
     autotrain_token: str
 
     def __post_init__(self):
-        try:
-            config_fname = hf_hub_download(
-                repo_id=self.competition_id,
-                filename="conf.json",
-                use_auth_token=self.autotrain_token,
-                repo_type="dataset",
-            )
-            competition_desc = hf_hub_download(
-                repo_id=self.competition_id,
-                filename="COMPETITION_DESC.md",
-                use_auth_token=self.autotrain_token,
-                repo_type="dataset",
-            )
-            dataset_desc = hf_hub_download(
-                repo_id=self.competition_id,
-                filename="DATASET_DESC.md",
-                use_auth_token=self.autotrain_token,
-                repo_type="dataset",
-            )
-        except EntryNotFoundError:
-            raise Exception("Competition config not found. Please check the competition id.")
-        except Exception as e:
-            logger.error(e)
-            raise Exception("Hugging Face Hub is unreachable, please try again later.")
-
+        config_fname = hf_hub_download(
+            repo_id=self.competition_id,
+            filename="conf.json",
+            use_auth_token=self.autotrain_token,
+            repo_type="dataset",
+        )
+        competition_desc = hf_hub_download(
+            repo_id=self.competition_id,
+            filename="COMPETITION_DESC.md",
+            use_auth_token=self.autotrain_token,
+            repo_type="dataset",
+        )
+        dataset_desc = hf_hub_download(
+            repo_id=self.competition_id,
+            filename="DATASET_DESC.md",
+            use_auth_token=self.autotrain_token,
+            repo_type="dataset",
+        )
         self.config = self.load_config(config_fname)
         self.competition_desc = self.load_md(competition_desc)
         self.dataset_desc = self.load_md(dataset_desc)
